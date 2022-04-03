8ccecf5fadcb716b44ce87d7d876e5fe5ed7260e
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-04-04 02:52:45 +0600

    home page finished

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Review from '../Review/Review';
+import Reviews from '../Reviews/Reviews';
 import "./Home.css"
 
 const Home = () => {
@@ -8,15 +8,15 @@ const Home = () => {
             {/* Banner Section  */}
             <section className='d-flex justify-content-around m-5'>
                 <div className='p-5 m-5'>
-                    <h1 className='pt-5 text-danger'>Spiderman-No Way Home</h1>
-                    <h4>Here you can see the reactions of people after watching the movie Spiderman-No Way Home</h4>
+                    <h1 className='pt-5 text-danger fw-bold'>Spiderman-No Way Home</h1>
+                    <h4>Here you can see the reactions of people after watching the movie <span className='text-danger'>Spiderman-No Way Home</span></h4>
                 </div>
                 <div>
                     <img className='poster' src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg" alt="" />
                 </div>
             </section>
 
-            <Review></Review>
+            <Reviews></Reviews>
         </div>
     );
 };
--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+import "./Review.css"
 
-const Review = () => {
+const Review = (props) => {
+    const { name, review, rating } = props.reviewer;
     return (
         <div>
-            <h2>Review</h2>
+            <Col>
+                <Card>
+                    <Card.Body className='review'>
+                        <Card.Title>
+                            <h2>{name}</h2>
+                        </Card.Title>
+                        <Card.Text className='fw-bold'>
+                            {review}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer className='fw-bold'>
+                        Rating: {rating}
+                    </Card.Footer>
+                </Card>
+            </Col>
         </div>
     );
 };
