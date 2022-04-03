c3baa3382c0b8c315d6d388527948c2fd47dd1ed
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-04-04 03:38:01 +0600

    see all btn done

--- src/Component/Reviews/Reviews.js
+++ src/Component/Reviews/Reviews.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import useReview from '../Hook/Hook';
 import Review from '../Review/Review';
 
 const Reviews = () => {
     const [reviews] = useReview();
     const newReviews = reviews.slice(0, 3);
+    const navigate = useNavigate();
+    const allReview = () => {
+        const path = "/allReview";
+        navigate(path);
+    }
     return (
         <div className='container mb-4'>
             <h1 className='text-danger my-5'>Movie Review (3)</h1>
@@ -14,6 +20,7 @@ const Reviews = () => {
                     newReviews.map(review => <Review key={review.id} reviewer={review}></Review>)
                 }
             </Row>
+            <button onClick={allReview} className='btn btn-danger fw-bold'>See All Reviews</button>
         </div>
     );
 };
