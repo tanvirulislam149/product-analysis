166b272c4ec57218495d73c5432f333b68d040a5
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-04-04 12:47:54 +0600

    chart added

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,9 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => setData(data));
+    }, [])
+
+    function FirstChart() {
+        return (
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                    width={600}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 20,
+                        left: 0,
+                        bottom: 0
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area
+                        type="monotone"
+                        dataKey="revenue"
+                        stackId="1"
+                        stroke="#82ca9d"
+                        fill="#82ca9d"
+                    />
+                    <Area
+                        type="monotone"
+                        dataKey="investment"
+                        stackId="1"
+                        stroke="#ffc658"
+                        fill="#ffc658"
+                    />
+                </AreaChart>
+            </ResponsiveContainer>
+        );
+    }
+
+
+    function SecondChart() {
+        return (
+            <ResponsiveContainer width="100%" height={400}>
+                <BarChart
+                    width={600}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 20,
+                        left: 20,
+                        bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+            </ResponsiveContainer>
+
+        );
+    }
+
+
     return (
         <div>
-            <h3>Dashboard</h3>
+            <h1>Dashboard</h1>
+            <Container>
+                <Row>
+                    <Col xs={12} md={6}>
+                        <FirstChart></FirstChart>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <SecondChart></SecondChart>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
